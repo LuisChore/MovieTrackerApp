b5e7ca701f227490450cee017bf84ab0ce5ec532
javarmgar <javarmgar@gmail.com>
2024-09-18 16:10:56 -0600

    removing unused fields

--- app/src/main/java/com/example/movietrackerapp/model/remote/service/response/MovieResponse.kt
+++ app/src/main/java/com/example/movietrackerapp/model/remote/service/response/MovieResponse.kt
@@ -3,10 +3,5 @@ package com.example.movietrackerapp.model.remote.service.response
 import com.google.gson.annotations.SerializedName
 
 data class MovieResponse (
-
-    @SerializedName("page"      	) var page     	: Int?           	= null,
     @SerializedName("results"   	) var results  	: ArrayList<Results> = arrayListOf(),
-    @SerializedName("total_pages"   ) var totalPages   : Int?           	= null,
-    @SerializedName("total_results" ) var totalResults : Int?           	= null
-
 )
